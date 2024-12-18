e829a6bf5a4078f4000c4c151db9056266ff33a1
COLOMBO MICHELE <COLOMBO.21191>
2024-12-18 13:27:27 +0100

    progetto aggiornato con parte grafica

--- ProjParcheggioThread/ProjParcheggioThread/Form1.Designer.cs
+++ ProjParcheggioThread/ProjParcheggioThread/Form1.Designer.cs
@@ -28,16 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             btnStart = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox99 = new PictureBox();
+            pictureBox000 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox600 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox88 = new PictureBox();
+            pictureBox9000 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox110 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox999 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox99).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox000).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox600).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox88).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9000).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox110).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox999).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(161, 130);
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(75, 141);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(65, 25);
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
@@ -45,7 +77,7 @@
             // 
             btnStart.BackColor = Color.Lime;
             btnStart.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            btnStart.Location = new Point(419, 189);
+            btnStart.Location = new Point(124, 189);
             btnStart.Name = "btnStart";
             btnStart.Size = new Size(108, 56);
             btnStart.TabIndex = 1;
@@ -53,15 +85,203 @@
             btnStart.UseVisualStyleBackColor = false;
             btnStart.Click += btnStart_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(365, 55);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(88, 158);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(469, 55);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(88, 158);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            pictureBox2.Visible = false;
+            // 
+            // pictureBox99
+            // 
+            pictureBox99.Image = (Image)resources.GetObject("pictureBox99.Image");
+            pictureBox99.Location = new Point(459, 55);
+            pictureBox99.Name = "pictureBox99";
+            pictureBox99.Size = new Size(4, 158);
+            pictureBox99.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox99.TabIndex = 4;
+            pictureBox99.TabStop = false;
+            // 
+            // pictureBox000
+            // 
+            pictureBox000.Image = (Image)resources.GetObject("pictureBox000.Image");
+            pictureBox000.Location = new Point(562, 55);
+            pictureBox000.Name = "pictureBox000";
+            pictureBox000.Size = new Size(4, 158);
+            pictureBox000.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox000.TabIndex = 6;
+            pictureBox000.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(572, 55);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(88, 158);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Visible = false;
+            // 
+            // pictureBox600
+            // 
+            pictureBox600.Image = (Image)resources.GetObject("pictureBox600.Image");
+            pictureBox600.Location = new Point(666, 55);
+            pictureBox600.Name = "pictureBox600";
+            pictureBox600.Size = new Size(4, 158);
+            pictureBox600.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox600.TabIndex = 8;
+            pictureBox600.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(676, 55);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(88, 158);
+            pictureBox4.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            pictureBox4.Visible = false;
+            // 
+            // pictureBox88
+            // 
+            pictureBox88.Image = (Image)resources.GetObject("pictureBox88.Image");
+            pictureBox88.Location = new Point(365, 210);
+            pictureBox88.Name = "pictureBox88";
+            pictureBox88.Size = new Size(399, 10);
+            pictureBox88.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox88.TabIndex = 9;
+            pictureBox88.TabStop = false;
+            // 
+            // pictureBox9000
+            // 
+            pictureBox9000.Image = (Image)resources.GetObject("pictureBox9000.Image");
+            pictureBox9000.Location = new Point(666, 219);
+            pictureBox9000.Name = "pictureBox9000";
+            pictureBox9000.Size = new Size(4, 158);
+            pictureBox9000.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox9000.TabIndex = 16;
+            pictureBox9000.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(676, 219);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(88, 158);
+            pictureBox8.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox8.TabIndex = 15;
+            pictureBox8.TabStop = false;
+            pictureBox8.Visible = false;
+            // 
+            // pictureBox110
+            // 
+            pictureBox110.Image = (Image)resources.GetObject("pictureBox110.Image");
+            pictureBox110.Location = new Point(562, 219);
+            pictureBox110.Name = "pictureBox110";
+            pictureBox110.Size = new Size(4, 158);
+            pictureBox110.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox110.TabIndex = 14;
+            pictureBox110.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(572, 219);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(88, 158);
+            pictureBox7.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox7.TabIndex = 13;
+            pictureBox7.TabStop = false;
+            pictureBox7.Visible = false;
+            // 
+            // pictureBox999
+            // 
+            pictureBox999.Image = (Image)resources.GetObject("pictureBox999.Image");
+            pictureBox999.Location = new Point(459, 219);
+            pictureBox999.Name = "pictureBox999";
+            pictureBox999.Size = new Size(4, 158);
+            pictureBox999.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox999.TabIndex = 12;
+            pictureBox999.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(469, 219);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(88, 158);
+            pictureBox6.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            pictureBox6.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(365, 219);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(88, 158);
+            pictureBox5.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            pictureBox5.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox9000);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox110);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox999);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox88);
+            Controls.Add(pictureBox600);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox000);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox99);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(btnStart);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox99).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox000).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox600).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox88).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9000).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox110).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox999).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -70,5 +290,20 @@
 
         private Label label1;
         private Button btnStart;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox99;
+        private PictureBox pictureBox000;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox600;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox88;
+        private PictureBox pictureBox9000;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox110;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox999;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
     }
 }
--- ProjParcheggioThread/ProjParcheggioThread/Form1.cs
+++ ProjParcheggioThread/ProjParcheggioThread/Form1.cs
@@ -7,20 +7,28 @@ namespace ProjParcheggioThread
 {
     public partial class Form1 : Form
     {
-        const int maxPosti = 5;  // Numero massimo di posti disponibili nel parcheggio
-        Semaphore parcheggio;     // Per gestire il numero di posti disponibili
-        Semaphore uscita;         // Per gestire l'uscita una macchina alla volta
+        const int maxPosti = 8;
+        Semaphore parcheggio;
+        Semaphore uscita;
         Random random;
         int nMacchine;
-        List<Thread> macchine;    // Lista per memorizzare i thread delle macchine
+        List<Thread> macchine;
+        List<PictureBox> immagini;
+        List<bool> postiOccupati;
 
         public Form1()
         {
             InitializeComponent();
-            parcheggio = new Semaphore(maxPosti, maxPosti);  // Impostiamo il numero massimo di posti
-            uscita = new Semaphore(1, 1);  // Solo una macchina può uscire alla volta
+            parcheggio = new Semaphore(maxPosti, maxPosti);
+            uscita = new Semaphore(1, 1);
             random = new Random();
-            macchine = new List<Thread>();  // Lista di macchine
+            macchine = new List<Thread>();
+            immagini = new List<PictureBox>()
+            {
+                pictureBox1, pictureBox2, pictureBox3, pictureBox4,
+                pictureBox5, pictureBox6, pictureBox7, pictureBox8
+            };
+            postiOccupati = new List<bool>(new bool[maxPosti]);
             label1.Text = "Numero di macchine: " + macchine.Count;
         }
 
@@ -31,30 +39,58 @@ namespace ProjParcheggioThread
                 MessageBox.Show("Non è possibile entrare. Il parcheggio è pieno.");
                 return;
             }
+            int postoLibero = TrovaPostoLiberoCasuale();
 
-            Thread macchina = new Thread(new ParameterizedThreadStart(MacchinaEntra));
+            if (postoLibero == -1)
+            {
+                MessageBox.Show("Non ci sono posti liberi.");
+                return;
+            }
 
+            Thread macchina = new Thread(MacchinaEntra);
             macchine.Add(macchina);
 
+            postiOccupati[postoLibero] = true;
             nMacchine = macchine.Count;
 
-            macchina.Start(nMacchine);
+            immagini[postoLibero].Visible = true;
+
+            macchina.Start(postoLibero); 
         }
 
-        private void MacchinaEntra(object macchinaId)
+        private int TrovaPostoLiberoCasuale()
         {
-            int id = (int)macchinaId;
+            List<int> postiLibero = new List<int>();
+
+            for (int i = 0; i < maxPosti; i++)
+            {
+                if (!postiOccupati[i])
+                {
+                    postiLibero.Add(i);
+                }
+            }
+
+            if (postiLibero.Count > 0)
+            {
+                return postiLibero[random.Next(postiLibero.Count)];
+            }
+
+            return -1;
+        }
+
+        private void MacchinaEntra(object postoId)
+        {
+            int posto = (int)postoId;
             try
             {
-
-                parcheggio.WaitOne(); 
+                parcheggio.WaitOne();
 
                 AggiornaLabelMacchine();
 
-                int tempoPermanenza = random.Next(500, 2000);
+                int tempoPermanenza = random.Next(2500, 4500);
                 Thread.Sleep(tempoPermanenza);
 
-                EsciDalParcheggio(id);
+                EsciDalParcheggio(posto);
             }
             finally
             {
@@ -62,7 +98,7 @@ namespace ProjParcheggioThread
             }
         }
 
-        private void EsciDalParcheggio(int id)
+        private void EsciDalParcheggio(int posto)
         {
             bool uscitaCompletata = false;
 
@@ -72,25 +108,25 @@ namespace ProjParcheggioThread
                 {
                     uscita.WaitOne();
 
-
-                    int tempoUscita = random.Next(1000, 3000); 
+                    int tempoUscita = random.Next(2000, 3000);
                     Thread.Sleep(tempoUscita);
+
                     macchine.RemoveAll(m => m.ManagedThreadId == Thread.CurrentThread.ManagedThreadId);
                     AggiornaLabelMacchine();
-                    MessageBox.Show("Macchina " + id + " ha lasciato il parcheggio.");
+                    MessageBox.Show("Macchina al posto " + (posto + 1) + " sta lasciando il parcheggio.");
                     uscitaCompletata = true;
                 }
                 catch (ThreadInterruptedException)
                 {
-                    int attesa = random.Next(2000, 4000);  
+                    int attesa = random.Next(1500, 4000);
                     Thread.Sleep(attesa);
                 }
                 finally
                 {
                     uscita.Release();
+                    AggiornaImmagineMacchine(posto);
                 }
             }
-
         }
 
         private void AggiornaLabelMacchine()
@@ -99,6 +135,15 @@ namespace ProjParcheggioThread
             {
                 label1.Text = "Numero di macchine: " + macchine.Count;
             }));
+        }
+
+        private void AggiornaImmagineMacchine(int posto)
+        {
+            immagini[posto].Invoke(new MethodInvoker(delegate
+            {
+                immagini[posto].Visible = false;
+            }));
+            postiOccupati[posto] = false;
         }
     }
 }
